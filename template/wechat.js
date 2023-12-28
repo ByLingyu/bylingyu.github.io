e66aed6e4446054fb9fe7d334ff81c8a64ef0063
ByLingyu <1324332313@qq.com>
2023-12-28 11:48:31 +0800

    1111

---
+++ template/wechat.js
@@ -0,0 +1,31 @@
+function Get(){
+  var a=location.search.substr(1).split("&"),b={},c,i
+  for(i in a){
+    c=a[i].split("=")
+    if(c.length && c[0] !== "") b[decodeURIComponent(c.shift())]=decodeURIComponent(c.join("="))
+  }
+  return b
+}
+function btnReturn() {
+    const url = '../index/index';
+    wx.miniProgram.navigateBack({});
+    console.log("返回了")
+}
+function btnMake(){
+    var myAudio = document.getElementById('audio_music');
+    if(myAudio){myAudio.pause();}
+    if(data_type == 1){
+        var url = "../make/make?type=user&id="+ id +"&public=1" ;
+    }else{
+        var url = "../make/make?id=" + Get()['demo_id'];
+    }
+    console.log("小程序制作url：" + url);
+    wx.miniProgram.navigateTo({
+        url: url
+    });
+    console.log("点击跳转制作界面了");
+}
+function btnMakeData(){
+    this.btnMakeUrl();
+    console.log("点击立即制作文案了")
+}
